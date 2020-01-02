35fbc641614a9f84ac6842b197180f65aca9d440
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 18:04:10 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
  
   epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:Capulet][:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   epic_tragedy
 end
